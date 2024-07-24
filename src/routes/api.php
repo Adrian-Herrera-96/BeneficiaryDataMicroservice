bd843cbde8febfb483a947810e5dce5517a35fc8
Cristhian Leandro Góngora Torrez <crisgongor7@gmail.com>
2024-07-24 10:48:43 -0400

    Ruta de API para crear Persona

--- src/routes/api.php
+++ src/routes/api.php
@@ -9,3 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/persons', [PersonController::class, 'index']);
+Route::post('/persons', [PersonController::class, 'store']);
